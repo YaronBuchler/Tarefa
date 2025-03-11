80f23b03381fa26f54585a5d77e0751bcd6e6b46
YaronBuchler <buchleryaron@gmail.com>
2025-03-10 23:17:39 -0300

    Novos etapas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputItem = document.getElementById("input-item");
-
+const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento)=> {
   evento.preventDefault();
@@ -8,4 +9,20 @@ botaoAdicionar.addEventListener("click", (evento)=> {
     alert("Por favor, insira um item!");
 return 
 }
+
+const ItemDaLista = document.createElement("li");
+const containerItemDaLista = document.createElement("div");
+containerItemDaLista.classList.add("container-item-lista");
+const inputCheckbox = document.createElement("input");
+inputCheckbox.type="checkbox";
+inputCheckbox.id="checkbox-"+contador++;
+const nomeItem = document.createElement("p");
+nomeItem.innerText = inputItem.value;
+
+container.ItemDaLista.appendchild(inputCheckbox);
+container.ItemDaLista.appendchild(nomeItem);
+
+itemDaLista.appendchild(containerItemDaLista)
+listaDeCompras.appendchild(itemDaLista);
+
 })
