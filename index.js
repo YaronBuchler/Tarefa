90cd3087a067a56293c3290b4b65451e8e9ad7f4
YaronBuchler <buchleryaron@gmail.com>
2025-03-10 22:33:43 -0300

    Começando

---
+++ index.js
@@ -0,0 +1,11 @@
+const inputItem = document.getElementById("input-item");
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento)=> {
+  evento.preventDefault();
+ if (inputItem.value===""){
+    alert("Por favor, insira um item!");
+return 
+}
+})
